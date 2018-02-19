3823282ea1ee7f56931fd762b396a1cf88218c50
Temp-975 <36461985+Temp-975@users.noreply.github.com>
2018-02-19 14:05:35 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 var pass = document.getElementById("pass");
 var display = document.getElementById("display");
 //setInterval(function() {display.innerHTML = pass.value;});
+pass.setAttribute("type", "text");
 pass.value = "aplejasjdmawodkj";
+pass.setAttribute("type", "password");
